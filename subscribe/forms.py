49d1786e2b35c20afb4f96c9f90bebdc32f4ffac
marc100s <marc@ellipsi.net>
2023-06-22 12:47:26 +0200

    Form module 2

--- subscribe/forms.py
+++ subscribe/forms.py
@@ -1,19 +1,39 @@
 from django import forms
 
-def validate_comma(value):
-  if "," in value:
-      raise forms.ValidationError("Invalid Last Name")
-  return value
+from subscribe.models import Subscribe
+from django.utils.translation import gettext_lazy as _
 
-class SubscribeForm(forms.Form):
-    first_name = forms.CharField(max_length=100, required=True, label="Enter Your First Name", help_text="Enter character only")
-    last_name = forms.CharField(max_length=100, disabled=False, label="Enter Your Last Name", validators=[validate_comma])
-    email = forms.EmailField(max_length=100, label="Enter Your Email")
+class SubscribeForm(forms.ModelForm):
+    class Meta:
+        model = Subscribe 
+        fields = '__all__'
+        # exclude = ('last_name',)
+        labels={
+            'first_name':_('Enter first name'),
+            'last_name':_('Enter last name'),
+            'email':_('Enter email')
+        }
+        help_texts={'first_name':_('Enter characters only')}
+        error_messages={
+            'first_name': {
+                'required' : _('You must enter your first name'),
+                }, 
+            'email': {
+                'required' : _('You must enter your email address'),
+            }
+        }
+        
+        # fields = ['first_name', 'last_name', 'email']
+
+# def validate_comma(value):
+#   if "," in value:
+#       raise forms.ValidationError("Invalid Last Name")
+#   return value
+
+
+    # first_name = forms.CharField(max_length=100, required=True, label="Enter Your First Name", help_text="Enter character only")
+    # last_name = forms.CharField(max_length=100, disabled=False, label="Enter Your Last Name", validators=[validate_comma])
+    # email = forms.EmailField(max_length=100, label="Enter Your Email")
     
-    # def clean_first_name(self):
-    #     data = self.cleaned_data['first_name']
-    #     if "," in data:
-    #         raise forms.ValidationError("Invalid First Name")
-    #     return data
-    
+
     
