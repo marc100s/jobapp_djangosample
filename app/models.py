c8fafccdef3170bf9ee8c61b53d4f142d6a4abd1
marc100s <marc@ellipsi.net>
2023-06-26 18:54:46 +0200

    Basic style without inheritance

--- app/models.py
+++ app/models.py
@@ -17,8 +17,12 @@ class Location(models.Model):
     zip =   models.CharField(max_length=200)
     
 class JobPost(models.Model):
+    JOB_TYPE_CHOICES =[
+        ('Full Time', 'Full Time'),
+        ('Part Time', 'Part Time'),
+    ]  
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     expiry = models.DateField(null=True, blank=True)
     salary = models.IntegerField()
@@ -26,6 +30,7 @@ class JobPost(models.Model):
     location = models.OneToOneField(Location, on_delete=models.CASCADE, null=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     skills = models.ManyToManyField(Skills)
+    type = models.CharField(max_length=200, null=False, choices=JOB_TYPE_CHOICES)
     
     def save(self, *args, **kwargs):
         if not self.id:
