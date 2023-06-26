1b3239558c20c50723ea18e17b5c7314bf2241fb
marc100s <marc@ellipsi.net>
2023-06-26 17:24:13 +0200

    Module image and text upload modified

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-#from app.views import hello, job_detail_page
-from app import views
+# from app.views import hello, job_detail
+from app import views 
+
 
 urlpatterns = [
     path('', views.job_list, name='jobs_home'),
--- jobapp/urls.py
+++ jobapp/urls.py
@@ -1,7 +1,7 @@
 """jobapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf.urls.static import static
-from django.conf import settings    
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('', include('subscribe.urls')),
     path('uploads/', include('uploadapp.urls')),
-     
-] + static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
